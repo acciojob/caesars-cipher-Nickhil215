90a4546c5d9de14afb15a452284174e74c72a422
Nikhil Vanamala <105160327+Nickhil215@users.noreply.github.com>
2023-04-14 11:17:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,19 +11,20 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   let decodedArr = [];  // Your Result goes here
-  // Only change code below this line
-	 for (let i = 0; i < str.length; i++) {
-    let charCode = str.charCodeAt(i);
-    if (charCode >= 65 && charCode <= 90) {
-      // Shift the character code by 13 places
-      charCode = (charCode - 65 + 13) % 26 + 65;
-    }
-    decodedArr += String.fromCharCode(charCode);
-  }
+    let decodedArr = [];  // Your Result goes here
+   // Only change code below this line
+      for (let i = 0; i < encodedStr.length; i++) {
+     let charCode = encodedStr.charCodeAt(i);
+     if (charCode >= 65 && charCode <= 90) {
+       // Shift the character code by 13 places
+       charCode = (charCode - 65 + 13) % 26 + 65;
+     }
+     decodedArr += String.fromCharCode(charCode);
+   }
+ 
+   return decodedArr ;//return decodedArr
+ }
 
-  return decodedArr ;//return decodedArr
-}
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
